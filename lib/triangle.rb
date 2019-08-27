05b9cebbcac94f261e74187d59a9884be5f108b6
Ismail Jalloh <ijalloh21@gmail.com>
2019-08-27 01:52:31 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,8 +8,17 @@ class Triangle
     @length_2 = length_2
     @length_1 = length_1
   end
+  
+  def validate_triangle
+    if length_1 == 0 || length_1 < 0 || length_2 < 0 || length_3 < 0 || (length_2/2 == (length_1 && length_3)) || (length_3/3 == (length_1 && length_2)) || ((length_2 + length_3) < length_1) || 
+      begin
+          raise TriangleError
+      end
+    end
+  end
     
   def kind
+    validate_triangle
     if (length_1 == length_2) && (length_2 == length_3)
       return :equilateral 
     elsif (length_1 == length_2) || (length_2 == length_3) || (length_3 == length_1)
